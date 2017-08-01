97384e794c1b75463288fe5e4d8abeea179f5c33
grahamRuttiman <ggruttiman@yahoo.com.au>
2017-08-01 12:57:17 +1000

    fix debugs

---
+++ Task-4/src/FixDebugOne1.java
@@ -0,0 +1,11 @@
+/**
+ * Created by jc432592 on 1/08/17
+ */
+public class FixDebugOne1
+{
+    /* This program displays a greeting */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+}
---
+++ Task-4/src/FixDebugOne4.java
@@ -0,0 +1,11 @@
+/**
+ * Created by jc432592 on 1/08/17.
+ */
+import javax.swing.JOptionPane;
+public class FixDebugOne4
+{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "First GUI program");
+    }
+}
